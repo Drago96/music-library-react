af1991259e29dbe4dc612248894ad2ccb4ac8414
Drago96 <dragproychev@gmail.com>
2019-03-12 00:57:42 +0200

    Added validation to login form

--- src/components/Authentication/LoginForm/LoginForm.tsx
+++ src/components/Authentication/LoginForm/LoginForm.tsx
@@ -2,8 +2,19 @@ import React from 'react';
 import { Formik, Field, Form } from 'formik';
 import { TextField } from 'formik-material-ui';
 import { Button, FormControl } from '@material-ui/core';
+import * as Yup from 'yup';
 
 import { useStyles } from './LoginForm.styles';
+
+const loginSchema = Yup.object().shape({
+  email: Yup.string()
+    .required('email is required')
+    .email('invalid email'),
+  password: Yup.string()
+    .required('password is required')
+    .min(6)
+    .max(24)
+});
 
 const LoginForm = () => {
   const classes = useStyles();
@@ -14,9 +25,10 @@ const LoginForm = () => {
       onSubmit={values => {
         // console.log(values);
       }}
+      validationSchema={loginSchema}
       render={({ submitForm }) => (
         <Form>
-          <FormControl fullWidth={true} margin={'normal'}>
+          <FormControl fullWidth={true} margin={'dense'}>
             <Field
               name="email"
               type="email"
@@ -24,7 +36,7 @@ const LoginForm = () => {
               component={TextField}
             />
           </FormControl>
-          <FormControl fullWidth={true} margin={'normal'}>
+          <FormControl fullWidth={true} margin={'dense'}>
             <Field
               type="password"
               label="Password"
@@ -32,7 +44,7 @@ const LoginForm = () => {
               component={TextField}
             />
           </FormControl>
-          <FormControl fullWidth={true} margin={'normal'}>
+          <FormControl fullWidth={true} margin={'dense'}>
             <Button
               onClick={submitForm}
               fullWidth={true}
