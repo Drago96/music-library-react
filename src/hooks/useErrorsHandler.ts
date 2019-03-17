eeb6fb985874732a50bb100bbecd82d0430cf4c2
Drago96 <dragproychev@gmail.com>
2019-03-17 18:31:28 +0200

    Fully implement error handling for login form

--- src/hooks/useErrorsHandler.ts
+++ src/hooks/useErrorsHandler.ts
@@ -1,13 +1,32 @@
 import { useCallback, useState } from 'react';
+import { ApolloError } from 'apollo-boost';
+import { head, get, flatten, map, values } from 'lodash';
 
 type UseErrorsHandlerHook = () => [string[], (errors: any) => void];
 
 export const useErrorsHandler: UseErrorsHandlerHook = () => {
   const [errors, setErrors] = useState<string[]>([]);
 
+  const parseGraphQLValidationErrors = useCallback(
+    validationErrors =>
+      flatten(
+        map(validationErrors, error => values(get(error, 'constraints')))
+      ),
+    []
+  );
+
   const translateGraphQLErrors = useCallback(
-    graphQLErrors => {
-      setErrors(['placeholder errors']);
+    (graphQLErrors: ApolloError) => {
+      const errorsObject = head(graphQLErrors.graphQLErrors);
+
+      const translatedErrors =
+        errorsObject.extensions.code === 'BAD_USER_INPUT'
+          ? parseGraphQLValidationErrors(
+              get(errorsObject, 'extensions.exception.errors')
+            )
+          : [errorsObject.message];
+
+      setErrors(translatedErrors);
     },
     [errors, setErrors]
   );
